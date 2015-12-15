c16c9940868147d2731337efd12e3457bb09d572
Alexander Martinz <eviscerationls@gmail.com>
2015-12-15 22:49:37 +0100

    namelessify
    Change-Id: I7e3056fbd798c9d243c6c7413703fc2cad111e34
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 BOARD_DTBTOOL_ARGS := --force-v3
 BOARD_KERNEL_CMDLINE += ramoops.mem_address=0x9ff00000 ramoops.mem_size=0x400000
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset 0x01000000
-TARGET_KERNEL_CONFIG := cyanogenmod_r7plus_defconfig
+TARGET_KERNEL_CONFIG := custom_r7plus_defconfig
 
 # Partition info
 BOARD_FLASH_BLOCK_SIZE := 131072
